b770dfbaef64915aca075f72cb7d74bd1ba13a2a
aarthy123 <vijitest.aarthy@gmail.com>
2022-05-17 20:33:33 +0530

    test update

---
+++ Day4Even/src/org/even/Even.java
@@ -0,0 +1,19 @@
+package org.even;
+
+public class Even {
+	public static void main(String[] args) {
+		int i=100;
+		for(i=1;i<=100;i++) {
+		if (i%2==0) {
+			System.out.println(i);
+			
+			
+		}
+		else
+		{
+			
+		}
+	}
+	}
+
+}
